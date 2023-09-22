94fcbe57897736f95185805285420556b02dc55d
qikerrs <129662698+qikerrs@users.noreply.github.com>
2023-09-23 01:03:27 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import { UserDetails } from "@/globalComponents/UserDetails";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function getTimeOfDay() {
   const currentHour = new Date().getHours();
