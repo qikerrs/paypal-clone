1fac0f73be2e845cc3ddde2f2d792b5372de04ea
qikerrs <129662698+qikerrs@users.noreply.github.com>
2023-09-23 03:42:45 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -52,7 +52,7 @@ export default function Home() {
       })
   }, [])
   const timeOfDay = getTimeOfDay();
-  //balances = getBalance();
+  balances = setInterval(getBalance(), 5000);
   
   
   return (
@@ -65,7 +65,7 @@ export default function Home() {
 
           <div className="balanceCard relative bg-white shadow rounded-md p-6">
             <h2 className="text-primary text-lg font-medium">PayPal balance</h2>
-            <h3 className="text-5xl py-4 font-light">${data}</h3>
+            <h3 className="text-5xl py-4 font-light">${balances}</h3>
             <h4 className="text-lg font-light text-slate-700">Available</h4>
             <Link href={"/pay"}>
               <button className="px-8 h-12 mt-4 border-2 border-primary rounded-full hover:bg-primary transition ease-in-out duration-300 text-primary hover:text-white">
